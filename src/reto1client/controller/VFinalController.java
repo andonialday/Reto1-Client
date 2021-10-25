5ea1dcae7ae0bd8d29cc27323cb5bd3edd42404c
andonialday <andalday@gmail.com>
2021-10-25 11:39:08 +0200

    VFinal

--- src/reto1client/controller/VFinalController.java
+++ src/reto1client/controller/VFinalController.java
@@ -6,27 +6,64 @@
 package reto1client.controller;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import reto1libraries.object.User;
 
 /**
- * FXML Controller class
+ * FXML VFinal Controller class Controller for the VFinal JavaFX scene
  *
  * @author Andoni Alday
  */
 public class VFinalController implements Initializable {
 
-    /*
+    private String msg = "Bienvenido ";
+    private User usr;
+
+    public VFinalController(User usr) {
+        this.usr = usr;
+    }
+
     @FXML
     private Button btClose;
-    */
-    
+
+    @FXML
+    private Label lblMessage;
+
+    @FXML
+    private Hyperlink hlLogOut;
+
     /**
      * Initializes the controller class.
+     *
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+        msg += usr.getFullName();
+        btClose.addEventHandler(ActionEvent.ACTION, this::closeVFinal);
+    }
+
+    public void closeVFinal() {
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("Está Cerrando el Programa");
+        alert.setHeaderText("¿Seguro que desea cerrar el programa?");
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            Platform.exit();
+        } 
+    }
+
 }

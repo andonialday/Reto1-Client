35cae035ee66beb3e782cd972dd536812e4e99c7
andonialday <andalday@gmail.com>
2021-11-02 10:42:26 +0100

    Ready (?)

--- src/reto1client/controller/VFinalController.java
+++ src/reto1client/controller/VFinalController.java
@@ -5,21 +5,22 @@
  */
 package reto1client.controller;
 
-import java.net.URL;
+import java.io.IOException;
 import java.util.Optional;
-import java.util.ResourceBundle;
+import java.util.logging.Logger;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import reto1libraries.object.User;
 
 /**
@@ -28,10 +29,10 @@ import reto1libraries.object.User;
  * @version 1.2
  * @author Andoni Alday
  */
-public class VFinalController implements Initializable {
+public class VFinalController {
 
     @FXML
-    private Button btClose;
+    private Button btnClose;
 
     @FXML
     private Label lblMessage;
@@ -39,30 +40,26 @@ public class VFinalController implements Initializable {
     @FXML
     private Hyperlink hlLogOut;
 
+    private Stage stage;
     private String msg;
     private User usr;
+    private Logger logger;
 
     /**
-     * Constructor for the VFinal Controller
-     */
-    public VFinalController() {
-    }
-
-    /**
-     * Initializes the controller class.
      *
-     * @param url
-     * @param rb
+     * @param root
      */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-            
-//receiveData();
+    public void initStage(Parent root) {
+        logger.info("Initializing Post SignIn process ...");
+        msg = "Bienvenido " + usr.getFullName();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("VFinal");
+        stage.setResizable(false);
         lblMessage.setText(msg);
-        btClose.setMnemonicParsing(true);
-        btClose.setText("_Close");
-        btClose.setOnAction(this::closeVFinal);
+        btnClose.setMnemonicParsing(true);
+        btnClose.setText("_Close");
+        btnClose.setOnAction(this::closeVFinal);
         hlLogOut.setOnAction(this::logOut);
     }
 
@@ -73,11 +70,13 @@ public class VFinalController implements Initializable {
      * @param event the event linked to clicking on the button;
      */
     public void closeVFinal(ActionEvent event) {
+        logger.info("Requesting confirmation for application closing...");
         Alert alert = new Alert(AlertType.CONFIRMATION);
         alert.setTitle("Está Cerrando el Programa");
         alert.setHeaderText("¿Seguro que desea cerrar el programa?");
         Optional<ButtonType> result = alert.showAndWait();
         if (result.get() == ButtonType.OK) {
+            logger.info("Closing the application");
             Platform.exit();
         }
     }
@@ -89,23 +88,38 @@ public class VFinalController implements Initializable {
      * @param event the event linked to clicking on the button;
      */
     public void logOut(ActionEvent event) {
+        logger.info("Requesting confirmation for Signing Out...");
         Alert alert = new Alert(AlertType.CONFIRMATION);
         alert.setTitle("Cerrando Sesión");
         alert.setHeaderText("¿Seguro que desea cerrar sesión?");
         Optional<ButtonType> result = alert.showAndWait();
         if (result.get() == ButtonType.OK) {
-
+            logger.info("Signing Out");
+        /*    FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("reto1client.view/VSignIn.fxml"));
+            Parent root = (Parent) loader.load();
+            VSignInController controller = ((VSignInController) loader.getController());
+            controller.setStage(this.stage);
+            controller.initStage(root);
+            */
         }
+    }
+
+    // Setters
+    /**
+     *
+     * @param user
+     */
+    public void setUser(User user) {
+        this.usr = user;
     }
 
     /**
      *
-     * @param event
+     * @param primaryStage
      */
-    private void receiveData(Event event) {
-        Node node = (Node) event.getSource();
-        this.usr = (User) node.getUserData();
-        this.msg = "Bienvenido " + usr.getFullName();
+    public void setStage(Stage primaryStage) {
+        this.stage = primaryStage;
     }
 
 }
